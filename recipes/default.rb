887de7937356f093c2dca36221d6c2175cab6a6a
Jeremy Seago <seagoj@gmail.com>
2013-09-08 19:19:06 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@ include_recipe 'apt'
 package 'git'
 
 directory node[:lib][:lib_dir] do
-    not_if File.exists?(node[:lib][:lib_dir])
+    not_if {File.exists?(node[:lib][:lib_dir])}
     owner node[:lib][:user]
     group node[:lib][:group]
     action :create
