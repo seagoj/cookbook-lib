fe7c27cffe65bff56f227a751dd53699ccb08b8d
Jeremy Seago <seagoj@gmail.com>
2013-09-19 14:17:36 -0500

    renames bootstrap destination to Bootstrap

--- recipes/bootstrap.rb
+++ recipes/bootstrap.rb
@@ -1,6 +1,6 @@
 include_recipe 'lib'
 
-git "#{node[:lib][:lib_dir]}/bootstrap" do
+git "#{node[:lib][:lib_dir]}/Bootstrap" do
   not_if {File.exists?("#{node[:lib][:lib_dir]}/bootstrap")}
   repository "git://github.com/twbs/bootstrap.git"
   reference "master"
