be1b0afa4ef995f336f3424331ddfbb9d20a443c
Jeremy Seago <seagoj@gmail.com>
2013-09-21 09:17:53 -0500

    Create lib directory if not doesn't exist

--- recipes/devtools-self.rb
+++ recipes/devtools-self.rb
@@ -1,4 +1,14 @@
+
+
 unless File.exists?("#{node[:lib][:lib_dir]}/Devtools")
+    unless File.exists?(node[:lib][:lib_dir])
+        Directory node[:lib][:lib_dir] do
+            owner node[:lib][:user]
+            group node[:lib][:group]
+            action :create
+        end
+    end
+
     execute "link to files" do
         cwd node[:lib][:lib_dir]
         user node[:lib][:user]
